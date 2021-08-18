630be05883651a49fe8b7f2c73f3649710257d4a
Philippe Schottey <phschotte@gmail.com>
2021-08-18 15:58:12 +0200

    feat: Optimalizatie voor 10 sec regel

--- wwoom-event-processor/src/main/java/be/vlaanderen/event/wwoom/StreamLambdaHandler.java
+++ wwoom-event-processor/src/main/java/be/vlaanderen/event/wwoom/StreamLambdaHandler.java
@@ -16,20 +16,19 @@ public class StreamLambdaHandler implements RequestStreamHandler {
 
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
-        } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
-            e.printStackTrace();
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
-
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
-        //handler.proxyStream(inputStream, outputStream, context);
+
+        if (handler == null) {
+            try {
+                handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+            } catch (ContainerInitializationException e) {
+                // if we fail here. We re-throw the exception to force another cold start
+                e.printStackTrace();
+                throw new RuntimeException("Could not initialize Spring Boot application", e);
+            }
+        }
 
         new NotificationHandler().handleRequest(inputStream, outputStream, context);
     }
